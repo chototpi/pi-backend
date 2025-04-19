42f596ee0779120efae5d03785447c5a929fc590
chototpi <moonbix.bnb@gmail.com>
2025-04-19 14:37:16 +0700

    Update test.js

--- routes/test.js
+++ routes/test.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.json({ message: 'Test route is working!' });
+  res.send('test route ok');
 });
 
 module.exports = router;
