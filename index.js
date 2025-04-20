0932fe87e93a4cccfb0133df88d648b1a428a24f
chototpi <moonbix.bnb@gmail.com>
2025-04-21 05:44:04 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 import fetch from 'node-fetch';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 const app = express();
+app.use(cors());
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
