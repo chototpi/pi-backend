21ac261a0c7fb65c76ed558e74f4b216a687ca3f
Chợ Tốt Pi <moonbix.bnb@gmail.com>
2025-07-01 09:36:24 +0700

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors({
-  origin: "https://chototpi.site"
+  origin: "https://payofpi.click"
 }));
 app.use(express.json());
 // Xóa client vì không sử dụng MongoDB native driver trong route /post/:id
