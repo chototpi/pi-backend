83b22fc42490b22a993fb46b2822775d9cadf596
Chợ Tốt Pi <moonbix.bnb@gmail.com>
2025-07-25 19:50:22 +0700

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors({
-  origin: "https://payofpi.click"
+  origin: "https://testnet.chototpi.site"
 }));
 app.use(express.json());
 // Xóa client vì không sử dụng MongoDB native driver trong route /post/:id
@@ -60,7 +60,7 @@ app.post("/approve-payment", async (req, res) => {
     const response = await fetch(`https://api.minepi.com/v2/payments/${paymentId}/approve`, {
       method: "POST",
       headers: {
-        Authorization: `Key ${process.env.PI_API_MAINNET}`,
+        Authorization: `Key ${process.env.PI_API_KEY}`,
         "Content-Type": "application/json"
       },
       body: JSON.stringify({})  // NEW body each time
@@ -95,7 +95,7 @@ app.post("/complete-payment", async (req, res) => {
     const response = await fetch(`https://api.minepi.com/v2/payments/${paymentId}/complete`, {
       method: "POST",
       headers: {
-        Authorization: `Key ${process.env.PI_API_MAINNET}`,
+        Authorization: `Key ${process.env.PI_API_KEY}`,
         "Content-Type": "application/json"
       },
       body: JSON.stringify({ txid }) // <-- Thêm txid vào body
@@ -115,26 +115,6 @@ app.post("/complete-payment", async (req, res) => {
     console.error("Complete ERROR:", err);
     res.status(500).json({ error: "Server error (complete)" });
   }
-});
-
-app.get("/pinet/meta", (req, res) => {
-  const pathname = req.query.pathname || "/";
-
-  // Tùy biến theo đường dẫn nếu bạn có nhiều page
-  let metadata = {
-    title: "Pay Of Pi - Cổng thanh toán Pi",
-    description: "Thanh toán hoá đơn và nạp tiền bằng Pi Network tại Việt Nam.",
-    image: "https://payofpi7677.pinet.com/paylogo.png", // ảnh cần đúng URL truy cập được công khai
-    url: "https://payofpi7677.pinet.com" + pathname,
-  };
-
-  // Trả về DTO chuẩn cho PiNet
-  res.status(200).json({
-    title: metadata.title,
-    description: metadata.description,
-    image: metadata.image,
-    url: metadata.url,
-  });
 });
 
 app.listen(PORT, () => {
